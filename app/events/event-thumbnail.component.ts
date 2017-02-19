51f0f0761ad0e3bdb86ee318784b9f7da99ac613
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-02-19 18:14:22 -0500

    Simple class addition

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,12 +6,13 @@ import { Component, EventEmitter, Input, Output, style } from '@angular/core';
         .thumbnail { min-height: 210px; }
         .pad-left { padding-left: 20px; }
         .well div { color: red }
+        .yellow { color: yellow !important;}
     `],
     template: `
     <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [ngSwitch]="event?.time">
+        <div [class.yellow]="event?.time === '8:00 am'" [ngSwitch]="event?.time">
             Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
