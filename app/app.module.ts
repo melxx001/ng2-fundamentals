af5694efd07ed7e7aca12a14032233e8b2c75145
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-02-20 01:20:34 -0500

    Adding multiple pages to app

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/nav-bar.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 
@@ -16,7 +17,8 @@ import { ToastrService } from './common/toastr.service';
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
-        NavBarComponent
+        NavBarComponent,
+        EventDetailsComponent
     ],
     providers: [
         EventService,
