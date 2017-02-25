fb70a27b1ed1c0dcb9495c3b81527679ceff4889
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-02-25 03:26:29 -0500

    Validating Template-based Forms

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -4,23 +4,30 @@ import { Component  } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Component({
+    styles: [`
+        em { float: right; color: red; padding-left: 10px; }
+    `],
     template: `
         <h1>Login</h1>
         <hr>
         <div class="col-md-4">
-        <form #loginForm="ngForm" (ngSubmit)="login(loginForm)" autocomplete="off">
+        <form #loginForm="ngForm" (ngSubmit)="login(loginForm)" autocomplete="off" novalidate>
             <div class="form-group" >
             <label for="userName">User Name:</label>
-            <input (ngModel)="userName" name="userName" id="userName" type="text" class="form-control" placeholder="User Name..." />
+            <em *ngIf="loginForm.controls.userName?.invalid && (loginForm.controls.userName?.touched || mouseoverLogin)">Required</em>
+            <input required (ngModel)="userName" name="userName" id="userName" type="text" class="form-control" placeholder="User Name..." />
             </div>
             <div class="form-group" >
             <label for="password">Password:</label>
-            <input (ngModel)="password" name="password" id="password" type="password" class="form-control"placeholder="Password..." />
+            <em *ngIf="loginForm.controls.password?.invalid && (loginForm.controls.password?.touched || mouseoverLogin)">Required</em>
+            <input required (ngModel)="password" name="password" id="password" type="password" class="form-control"placeholder="Password..." />
             </div>
                 
-            <button type="submit" class="btn btn-primary">Login</button>
+            <span (mouseenter)="mouseoverLogin=true" (mouseleave)="mouseoverLogin=false">
+                <button type="submit" [disabled]="loginForm.invalid" class="btn btn-primary">Login</button>
+            </span>
             <button type="button" (click)="cancel()" class="btn btn-default">Cancel</button>
-        </form>
+        </form> 
         </div>
     `
 })
