940ced785a5bc3bb76c753af49e11f8f126266bd
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-02-26 10:44:57 -0500

    Deeper into Reactive Forms

--- app/routes.ts
+++ app/routes.ts
@@ -5,7 +5,8 @@ import {
     EventDetailsComponent,
     CreateEventComponent,
     EventRouteActivator,
-    EventsListResolve
+    EventsListResolve,
+    CreateSessionComponent
 } from './events/index';
 
 import { Error404Component } from './errors/404.component';
@@ -15,6 +16,7 @@ export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventsListComponent, resolve: { events: EventsListResolve } },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+    { path: 'events/session/new', component: CreateSessionComponent },
     { path: '404', component: Error404Component },
     { path: '', redirectTo: '/events', pathMatch: 'full' },
     { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
