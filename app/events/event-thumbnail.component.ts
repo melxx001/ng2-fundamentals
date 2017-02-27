1f7d09c9a1a2391202dbcea886439c809e853e93
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-02-27 02:20:03 -0500

    Using Built-In Pipes

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -13,15 +13,15 @@ import { Component, EventEmitter, Input, Output, style } from '@angular/core';
     `],
     template: `
     <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-        <h2>{{event?.name}}</h2>
-        <div>Date: {{event?.date}}</div>
+        <h2>{{event?.name | uppercase}}</h2>
+        <div>Date: {{event?.date | date: 'shortDate'}}</div>
         <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
             Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
             <span *ngSwitchDefault>(Normal Start)</span>
         </div>
-        <div [ngStyle]="getStartTimeStyle()">Price: \${{event?.price}}</div>
+        <div [ngStyle]="getStartTimeStyle()">Price: {{event?.price | currency: 'USD': true}}</div>
         <div *ngIf="event?.location">
             <span>Location: {{event?.location?.address}}</span>
             <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
@@ -33,21 +33,21 @@ import { Component, EventEmitter, Input, Output, style } from '@angular/core';
 export class EventThumbnailComponent {
     @Input() event: any;
     @Output() eventClick = new EventEmitter();
-    
-    getStartTimeClass() : any {
+
+    getStartTimeClass(): any {
         const earlyStart = this.event && this.event.time === '8:00 am';
         return {
-            yellow: earlyStart, 
+            yellow: earlyStart,
             bold: earlyStart
         }
     }
 
-    getStartTimeStyle() : any {
+    getStartTimeStyle(): any {
         const earlyStart = this.event && this.event.time === '8:00 am';
 
         return {
-            color: earlyStart ? 'pink' : 'lightgreen', 
-            'font-weight': earlyStart ? 'normal': 'bold'
+            color: earlyStart ? 'pink' : 'lightgreen',
+            'font-weight': earlyStart ? 'normal' : 'bold'
         }
     }
 }
