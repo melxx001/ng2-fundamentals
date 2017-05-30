0c82ae8eb1a75578aea59ed9489ef1326334f97a
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-05-30 06:01:45 -0400

    Integrating Authentication with the server

--- app/user/auth.service.ts
+++ app/user/auth.service.ts
@@ -1,17 +1,32 @@
+import {
+    Headers,
+    Http,
+    RequestOptions,
+    Response
+} from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/RX';
 import { User } from './user.model';
 
 @Injectable()
 export class AuthService {
     currentUser: User;
 
-    loginUser (userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            firstName: 'John',
-            lastName: 'Smith',
-            userName: userName
-        }   
+    constructor(private http: Http) { }
+
+    loginUser(userName: string, password: string): Observable<any> {
+        const headers = new Headers({ 'Content-Type': 'application/json' });
+        const options = new RequestOptions({ headers: headers });
+        const loginInfo = { username: userName, password };
+
+        return this.http.post('api/login', loginInfo, options)
+            .do((resp: Response) => {
+                if (resp) {
+                    this.currentUser = <User>resp.json().user;
+                }
+            }).catch(error => {
+                return Observable.of(false);
+            });
     }
 
     isAuthenticated() {
@@ -22,4 +37,5 @@ export class AuthService {
         this.currentUser.firstName = firstName;
         this.currentUser.lastName = lastName;
     }
+
 }
--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -11,32 +11,42 @@ import { AuthService } from './auth.service';
         <h1>Login</h1>
         <hr>
         <div class="col-md-4">
-        <form #loginForm="ngForm" (ngSubmit)="login(loginForm)" autocomplete="off" novalidate>
-            <div class="form-group" >
-            <label for="userName">User Name:</label>
-            <em *ngIf="loginForm.controls.userName?.invalid && (loginForm.controls.userName?.touched || mouseoverLogin)">Required</em>
-            <input required (ngModel)="userName" name="userName" id="userName" type="text" class="form-control" placeholder="User Name..." />
-            </div>
-            <div class="form-group" >
-            <label for="password">Password:</label>
-            <em *ngIf="loginForm.controls.password?.invalid && (loginForm.controls.password?.touched || mouseoverLogin)">Required</em>
-            <input required (ngModel)="password" name="password" id="password" type="password" class="form-control"placeholder="Password..." />
-            </div>
-                
-            <span (mouseenter)="mouseoverLogin=true" (mouseleave)="mouseoverLogin=false">
-                <button type="submit" [disabled]="loginForm.invalid" class="btn btn-primary">Login</button>
-            </span>
-            <button type="button" (click)="cancel()" class="btn btn-default">Cancel</button>
-        </form> 
+            <form #loginForm="ngForm" (ngSubmit)="login(loginForm)" autocomplete="off" novalidate>
+                <div class="form-group" >
+                    <label for="userName">User Name:</label>
+                    <em *ngIf="loginForm.controls.userName?.invalid && (loginForm.controls.userName?.touched || mouseoverLogin)">Required</em>
+                    <input required (ngModel)="userName" name="userName" id="userName" type="text" class="form-control" placeholder="User Name..." />
+                </div>
+                <div class="form-group" >
+                    <label for="password">Password:</label>
+                    <em *ngIf="loginForm.controls.password?.invalid && (loginForm.controls.password?.touched || mouseoverLogin)">Required</em>
+                    <input required (ngModel)="password" name="password" id="password" type="password" class="form-control"placeholder="Password..." />
+                </div>
+                    
+                <span (mouseenter)="mouseoverLogin=true" (mouseleave)="mouseoverLogin=false">
+                    <button type="submit" [disabled]="loginForm.invalid" class="btn btn-primary">Login</button>
+                </span>
+                <button type="button" (click)="cancel()" class="btn btn-default">Cancel</button>
+            </form>
+            <br>
+            <div *ngIf="loginInvalid" class="alert alert-danger">Invalid Login Info</div> 
         </div>
     `
 })
 export class LoginComponent {
+    loginInvalid = false;
+
     constructor(private authService: AuthService, private router: Router) { }
 
     login(formValues) {
-        this.authService.loginUser(formValues.value.userName, formValues.value.password);
-        this.router.navigate(['events']);
+        this.authService.loginUser(formValues.value.userName, formValues.value.password).subscribe(resp => {
+            if (!resp) {
+                this.loginInvalid = true;
+            } else {
+                this.loginInvalid = false;
+                this.router.navigate(['events']);
+            }
+        });
     }
 
     cancel() {
