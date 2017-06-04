4dd8a5f818aef8008e3ff0db522712a0cf41e7a3
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-06-04 04:48:09 -0400

    Testing Mock Calls

--- app/events/event-details/voter.service.spec.ts
+++ app/events/event-details/voter.service.spec.ts
@@ -20,5 +20,23 @@ describe('VoterService', () => {
       expect(session.voters.length).toBe(1);
       expect(session.voters[0]).toBe('john');
     });
+
+    it('should call http.delete with the right URL', () => {
+      const session = { id: 6, voters: ['joe', 'john'] };
+      mockHttp.delete.and.returnValue(Observable.of(false));
+
+      voterService.deleteVoter(3, <Sessions>session, 'joe');
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+    });
+  });
+
+  describe('addVoter', () => {
+    it('should call http.post with the right URL', () => {
+      const session = { id: 6, voters: ['joe'] };
+      mockHttp.post.and.returnValue(Observable.of(false));
+
+      voterService.addVoter(3, <Sessions>session, 'joe');
+      expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
+    });
   });
 });
