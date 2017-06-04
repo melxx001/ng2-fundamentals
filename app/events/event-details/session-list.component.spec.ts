f64e8d89d6baabbb673cbae5e03ec6d4f80197d9
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-06-04 05:03:05 -0400

    Testing Components with isolate tests

---
+++ app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,42 @@
+import { Sessions } from './../shared/event.model';
+import { SessionListComponent } from './session-list.component';
+
+describe('SessionListComponent', () => {
+  let component: SessionListComponent;
+  let mockAuthService;
+  let mockVoterService;
+
+  beforeEach(() => {
+    component = new SessionListComponent(mockAuthService, mockVoterService);
+  });
+
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+      component.sessions = <Sessions[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 2', level: 'intermediate' },
+        { name: 'session 3', level: 'beginner' }
+      ];
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
+      expect(component.visibleSessions.length).toBe(2);
+    });
+
+    it('should filter the sessions correctly', () => {
+      component.sessions = <Sessions[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 2', level: 'intermediate' },
+        { name: 'session 3', level: 'beginner' }
+      ];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
+      expect(component.visibleSessions[2].name).toBe('session 3');
+    });
+  });
+});
