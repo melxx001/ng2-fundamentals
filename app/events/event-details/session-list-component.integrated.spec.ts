a83a021417b2182b693b687cfec421f07effab45
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-06-05 01:18:50 -0400

    Testing Component with Deep Integrated Tests (Tests failing)

--- app/events/event-details/session-list-component.integrated.spec.ts
+++ app/events/event-details/session-list-component.integrated.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthService } from './../../user/auth.service';
-import { DebugElement, Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { Component, DebugElement } from '@angular/core';
 import { SessionListComponent } from './session-list.component';
 import { Sessions } from './../shared/event.model';
+import { UpvoteComponent } from './upvote.component';
 import { VoterService } from './voter.service';
-import { By } from '@angular/platform-browser';
+import { DurationPipe } from '../shared/duration.pipe';
+import { CollapsibleWellComponent } from '../../common/collapsible-well.component';
 
 describe('SessionListComponent', () => {
   let fixture: ComponentFixture<SessionListComponent>;
@@ -19,7 +22,12 @@ describe('SessionListComponent', () => {
 
       TestBed.configureTestingModule({
         imports: [],
-        declarations: [SessionListComponent],
+        declarations: [
+          SessionListComponent,
+          UpvoteComponent,
+          DurationPipe,
+          CollapsibleWellComponent
+        ],
         providers: [
           { provide: AuthService, useValue: mockAuthService },
           { provide: VoterService, useValue: mockVoterService }
@@ -34,5 +42,32 @@ describe('SessionListComponent', () => {
     component = fixture.componentInstance;
     debugEl = fixture.debugElement;
     element = fixture.nativeElement;
+  });
+
+  describe('Initial Display', () => {
+    it('should have the correct session title', () => {
+      component.sessions = [
+        {
+          id: 3,
+          name: 'Session 1',
+          presenter: 'Joe',
+          duration: 1,
+          level: 'beginner',
+          abstract: 'abstract',
+          voters: ['john', 'bob']
+        }
+      ];
+
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expect(element.querySelector('[well-title]').textContent).toContain(
+        'Session 1'
+      );
+    });
   });
 });
