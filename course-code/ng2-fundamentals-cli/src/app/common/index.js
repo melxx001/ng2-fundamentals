8d883453acc7e9d49780073f6252188f30802def
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2017-06-14 06:08:34 -0400

    Add cli version of the code

---
+++ course-code/ng2-fundamentals-cli/src/app/common/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jQuery_service_1 = require("./jQuery.service");
+exports.JQ_TOKEN = jQuery_service_1.JQ_TOKEN;
+var toastr_service_1 = require("./toastr.service");
+exports.TOASTR_TOKEN = toastr_service_1.TOASTR_TOKEN;
+var collapsible_well_component_1 = require("./collapsible-well.component");
+exports.CollapsibleWellComponent = collapsible_well_component_1.CollapsibleWellComponent;
+var simpleModal_component_1 = require("./simpleModal.component");
+exports.SimpleModalComponent = simpleModal_component_1.SimpleModalComponent;
+var modalTrigger_directive_1 = require("./modalTrigger.directive");
+exports.ModalTriggerDirective = modalTrigger_directive_1.ModalTriggerDirective;
+//# sourceMappingURL=index.js.map
---
+++ course-code/ng2-fundamentals-cli/src/app/events/shared/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var event_service_1 = require("./event.service");
+exports.EventService = event_service_1.EventService;
+var restricted_words_validator_1 = require("./restricted-words.validator");
+exports.restrictedWords = restricted_words_validator_1.restrictedWords;
+var duration_pipe_1 = require("./duration.pipe");
+exports.DurationPipe = duration_pipe_1.DurationPipe;
+//# sourceMappingURL=index.js.map
